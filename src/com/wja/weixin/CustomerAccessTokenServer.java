dc3addffeb8a958059eb4175ad8dfaea948c78ff
wjacl <wang_jian_an@139.com>
2017-09-24 22:07:39 +0800

    加入wechat4j

---
+++ src/com/wja/weixin/CustomerAccessTokenServer.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.wja.weixin;
+
+import org.sword.wechat4j.token.Token;
+import org.sword.wechat4j.token.server.CustomerServer;
+
+/**
+ * @author repo_user
+ * @date   2015年1月12日
+ */
+public class CustomerAccessTokenServer extends CustomerServer {
+
+	/* (non-Javadoc)
+	 * @see org.sword.wechat4j.token.DbAccessTokenServer#find()
+	 */
+	@Override
+	public String find() {
+		String accessToken = null;
+		//执行数据库操作
+//		String sql = "select cfgValue from cfg where cfg.cfgKey = 'access_token'";
+//		accessToken = DBUtil.query(sql);
+		return accessToken;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sword.wechat4j.token.DbAccessTokenServer#save()
+	 */
+	@Override
+	public boolean save(Token accessToken) {
+		//如果没有需要插入，如果有的就更新，假设已经有了数据库配置项
+//		String sql = "update cfg set cfg.cfgValue=" + accessToken.getToken() + 
+//				" where cfg.cfgKey= 'access_token'";
+//		DBUtil.execute(sql);
+		return true;
+	}
+}
---
+++ src/com/wja/weixin/Hello.java
@@ -0,0 +1,85 @@
+package com.wja.weixin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.sword.wechat4j.user.UserManager;
+
+/**
+ * Servlet implementation class Hello
+ */
+public class Hello extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+    
+    private static Logger logger = Logger.getLogger(Hello.class);
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello()
+    {
+        super();
+    }
+    
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        String result = "你好，wechat4j";
+        String op = request.getParameter("op");
+        if (StringUtils.isNotBlank(op))
+        {
+            // 主动发送客服消息
+            if (op.equals("1"))
+            {
+                sendMsg();
+                result = "已发送";
+            }
+        }
+        response.setCharacterEncoding("UTF-8");// 服务器编码
+        response.setHeader("content-type", "text/html;charset=UTF-8");// 浏览器编码
+        PrintWriter out = response.getWriter();
+        out.println(result);
+    }
+    
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        String result = "你好，wechat4j";
+        response.setCharacterEncoding("UTF-8");
+        response.setHeader("content-type", "text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println(result);
+    }
+    
+    /**
+     * 单个发送
+     */
+    private void sendMsg()
+    {
+        logger.info("主动发送消息demo");
+        // 获得关注者列表，发送给第一个人消息
+        UserManager userManager = new UserManager();
+        /*
+         * List<String> userList = userManager.subscriberList(); if (userList.size() > 0) { String toUserOpenId =
+         * userList.get(0).toString(); String content = "主动发送"; CustomerMsg customerMsg = new CustomerMsg(toUserOpenId);
+         * customerMsg.sendText(content); }
+         */
+    }
+    
+}
