44af6070c93f04a0b190fe547261a90fa01d9e7f
wjacl <wang_jian_an@139.com>
2017-11-06 19:11:14 +0800

    加入充值body

--- db/oracle/init02.sql
+++ db/oracle/init02.sql
@@ -8,6 +8,9 @@ VALUES('wx.download.public.url','公众号网页公开文件的下载url','/weix
 INSERT INTO t_sys_param(ID,name,VALUE,remark)
 VALUES('bzj.pay.body','保证金微信支付显示内容','保证金支付','保证金微信支付显示内容');
 
+INSERT INTO t_sys_param(ID,name,VALUE,remark)
+VALUES('cz.pay.body','充值微信支付显示内容','账户充值','充值微信支付显示内容');
+
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('follwer.category','经营类别','follwer.category',100,'0','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('fct1','厂家','1',1,'follwer.category','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('fct2','商家','2',1,'follwer.category','s',1);
