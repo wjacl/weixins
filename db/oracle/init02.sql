2f9671971ba653cfff0353837c803cde0cebdf32
wjacl <wang_jian_an@139.com>
2017-11-22 23:54:04 +0800

    工单前缀

--- db/oracle/init02.sql
+++ db/oracle/init02.sql
@@ -17,6 +17,9 @@ VALUES('mess.fb.plat.fee','信息全平台推送收费金额','5','信息全平�
 INSERT INTO t_sys_param(ID,name,VALUE,remark)
 VALUES('mess.prod.title.prefix','产品消息的标题前缀','新产品！','产品消息的标题前缀');
 
+INSERT INTO t_sys_param(ID,name,VALUE,remark)
+VALUES('mess.pd.title.prefix','派单消息的标题前缀','工单-','产品消息的标题前缀');
+
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('follwer.category','经营类别','follwer.category',100,'0','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('fct1','厂家','1',1,'follwer.category','s',1);
 INSERT INTO t_sys_dict(Id,name,value,ordno,pid,type,valid) VALUES('fct2','商家','2',2,'follwer.category','s',1);
